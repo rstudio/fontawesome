5d866bcd8b04588d83e6122f4c1b2943ab45b481
Richard Iannone <riannone@me.com>
2021-02-11 18:15:34 -0500

    Create shiny-test.R

---
+++ tests/manual_tests/shiny-test.R
@@ -0,0 +1,74 @@
+library(ggplot2)
+library(DT)
+library(fontawesome)
+
+# Load the ggplot2 package which provides
+# the 'mpg' dataset
+ui <- fluidPage(
+
+  titlePanel("Basic DataTable"),
+
+  # Create a new Row in the UI for selectInputs
+  fluidRow(
+
+    column(
+      width = 4,
+      selectInput(
+        inputId = "man",
+        label = tags$p(fa("car", fill = "purple"), "Manufacturer:"),
+        choices = c(
+          "All",
+          unique(as.character(mpg$manufacturer))))
+    ),
+
+    column(
+      width = 4,
+      selectInput(
+        inputId = "trans",
+        label = tags$p(fa("car", fill = "forestgreen"), "Transmission:"),
+        choices = c(
+          "All",
+          unique(as.character(mpg$trans))))
+    ),
+
+    column(
+      width = 4,
+      selectInput(
+        inputId = "cyl",
+        label = tags$p(fa("car", fill = "steelblue"), "Cylinders:"),
+        choices = c(
+          "All",
+          unique(as.character(mpg$cyl))))
+    )
+  ),
+
+  # Create a new row for the table.
+  fluidRow(
+    dataTableOutput("table")
+  )
+)
+
+# Define the server code
+# Load the ggplot2 package which provides
+# the 'mpg' dataset
+server <- function(input, output) {
+
+  # Filter data based on selections
+  output$table <- renderDataTable({
+
+    data <- mpg
+    if (input$man != "All") {
+      data <- data[data$manufacturer == input$man,]
+    }
+    if (input$cyl != "All") {
+      data <- data[data$cyl == input$cyl,]
+    }
+    if (input$trans != "All") {
+      data <- data[data$trans == input$trans,]
+    }
+    data
+  })
+}
+
+# Return a Shiny app object
+shinyApp(ui = ui, server = server)
