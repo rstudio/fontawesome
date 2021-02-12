fb50ed42a21d31155daefbf5610fc019ee7b4d2e
Richard Iannone <riannone@me.com>
2021-02-12 11:37:38 -0500

    Rewrite `fa_png()`

--- R/fa_png.R
+++ R/fa_png.R
@@ -1,55 +1,56 @@
 #' Create a PNG version of a FontAwesome icon
 #'
-#' Get a FontAwesome icon as a PNG file. We can
-#' optionally set the fill attribute before
-#' writing the PNG. Additionally, there is control
-#' over the output width and height (usually, icons
-#' are 512 by 512 pixels).
-#' @param name the name of the FontAwesome icon.
-#' @param file the path to the output file. If \code{NULL},
-#' then filename will take the short name of the icon (with
-#' a \code{.png} extension).
-#' @param fill an option to change the fill color of
-#' the icon.
-#' @param width the output width in pixels. If \code{NULL},
-#' then the output width will match that of the SVG's
-#' \code{viewBox}.
-#' @param height the output height in pixels. If \code{NULL},
-#' then the output height will match that of the SVG's
-#' \code{viewBox}.
+#' Get a FontAwesome icon as a PNG file. We can optionally set the fill
+#' attribute before writing the PNG. Additionally, there is control over the
+#' output width and height (usually, icons are 512 by 512 pixels).
+#'
+#' @inheritParams fa
+#' @param file the path to the output file. If `NULL`, then filename will
+#'   take the short name of the icon and a `.png` extension will be
+#'   applied.
+#' @param height,width The output height and width of the rendered PNG.
+#'   If nothing is provided then the output dimensions will match that of the
+#'   input SVG viewBox.
+#'
+#' @return A PNG file written to disk.
+#'
+#' @examples
+#' # Create a FontAwesome SVG icon as a
+#' # PNG file on disk
+#' fa_png(name = "r-project")
+#'
 #' @export
 fa_png <- function(name,
                    file = NULL,
                    fill = NULL,
-                   width = NULL,
-                   height = NULL) {
+                   fill_opacity = NULL,
+                   stroke = NULL,
+                   stroke_width = NULL,
+                   stroke_opacity = NULL,
+                   height = NULL,
+                   width = NULL) {
 
   # nocov start
 
-  # Obtain the inline SVG as a vector
-  svg <- fa(name = name, fill = fill)
-
-  # Stop function if the name doesn't match a
-  # FontAwesome icon name
-  if (svg == "") {
-    stop("The provided `name` doesn't correspond to that of a FontAwesome icon.",
-         call. = FALSE)
-  }
+  # Obtain the SVG
+  svg <-
+    fa(
+      name = name,
+      fill = fill,
+      fill_opacity = fill_opacity,
+      stroke = stroke,
+      stroke_width = stroke_width,
+      stroke_opacity = stroke_opacity
+    )
 
   # If `file` is `NULL` then construct a suitable
   # filename based on the icon's short name
   if (is.null(file)) {
-
     if (name %in% fa_tbl$full_name) {
-
-      short_name <-
-        fa_tbl[which(fa_tbl$full_name == name), ][1, 1]
-
+      short_name <- fa_tbl[which(fa_tbl$full_name == name), ][1, 1]
     } else if (name %in% fa_tbl$name) {
-
       short_name <- name
     }
-
     file <- paste0(short_name, ".png")
   }
 
@@ -58,9 +59,12 @@ fa_png <- function(name,
   if (requireNamespace("rsvg", quietly = TRUE)) {
 
     rsvg::rsvg_png(
-      svg = charToRaw(svg),
+      svg = charToRaw(as.character(svg)),
       file = file,
       width = width,
-      height = height)
-  } # nocov end
+      height = height
+    )
+  }
+
+  # nocov end
 }
