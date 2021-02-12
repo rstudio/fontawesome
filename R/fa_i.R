7e3d52bb953b3a828f6c9f2918c8d6640c129a96
Richard Iannone <riannone@me.com>
2021-02-12 15:47:59 -0500

    Create fa_i.R

---
+++ R/fa_i.R
@@ -0,0 +1,45 @@
+#' Generate a FontAwesome `<i>` tag
+#'
+#' @inheritParams fa
+#' @param class Additional classes to customize the style of the
+#' icon (see the usage examples for details on supported styles).
+#'
+#' @export
+fa_i <- function(name,
+                 class = NULL,
+                 ...) {
+
+  prefix <- "fa"
+  iconClass <- ""
+
+  if (!is.null(name)) {
+    prefix_class <- prefix
+    if (prefix_class == "fa" && name %in% font_awesome_brands) {
+      prefix_class <- "fab"
+    }
+    iconClass <- paste0(prefix_class, " ", prefix, "-", name)
+  }
+
+  if (!is.null(class)) {
+    iconClass <- paste(iconClass, class)
+  }
+
+  icon_tag <-
+    htmltools::tags$i(
+      class = iconClass,
+      role = "presentation",
+      `aria-label` = paste(name, "icon"),
+      ...
+    )
+
+  htmltools::htmlDependencies(icon_tag) <-
+    htmltools::htmlDependency(
+      name = "font-awesome",
+      version = "5.13.0",
+      src = "fontawesome",
+      package = "fontawesome",
+      stylesheet = c("css/all.min.css", "css/v4-shims.min.css")
+    )
+
+  htmltools::browsable(icon_tag)
+}
