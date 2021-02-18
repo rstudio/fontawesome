15ce42f9b2e2d115b71e199b3cd368722702a756
Richard Iannone <riannone@me.com>
2021-02-18 13:57:41 -0500

    Update `knit_print` method

--- R/knit_print.R
+++ R/knit_print.R
@@ -1,13 +1,19 @@
 #' Safely print any FontAwesome in R Markdown
-#' @description This facilitates printing of
-#' the FontAwesome icon within R Markdown.
-#' @param x an object containing the class
-#' `fontawesome`.
+#'
+#' This facilitates printing of the FontAwesome icon within R Markdown.
+#'
+#' @param x An object of class `fontawesome`.
+#' @param ... Any additional parameters.
+#'
 #' @keywords internal
-knit_print.fontawesome <- function(x, ...) { # nocov start
+#' @noRd
+knit_print.fontawesome <- function(x, ...) {
+
+  # nocov start
 
   if (requireNamespace("knitr", quietly = TRUE)) {
-
     knitr::knit_print(as.character(x), ...)
   }
-} # nocov end
+
+  # nocov end
+}
