a01fd3892e7fa9c72c7077623c3820e76e11ae77
christophe dervieux <christophe.dervieux@gmail.com>
2021-03-17 14:16:32 +0100

    working knit_print for pdf

--- R/knit_print.R
+++ R/knit_print.R
@@ -7,13 +7,34 @@
 #'
 #' @keywords internal
 #' @noRd
-knit_print.fontawesome <- function(x, ...) {
+knit_print.fontawesome <- function(x, ..., options, inline = FALSE) {
 
   # nocov start
 
   if (requireNamespace("knitr", quietly = TRUE)) {
-    knitr::knit_print(as.character(x), ...)
+    if (knitr::is_html_output()) {
+      NextMethod()
+    } else if (knitr::is_latex_output()) {
+      if (!requireNamespace("rsvg", quietly = TRUE)) {
+        stop("Using fontawesome in PDF output requires the rsvg package:\n",
+             " * It can be installed with `install.packages(\"rsvg\")`.",
+             call. = FALSE)
+      }
+      # create the icon in a tempfile and copy to fig.path for knitting
+      tmp_pdf <- tempfile("fa-icon-", fileext = ".pdf")
+      on.exit(unlink(tmp_pdf), add = TRUE)
+      raw_fa <- charToRaw(as.character(x))
+      rsvg::rsvg_pdf(raw_fa, file = tmp_pdf)
+      knitr_fig_path <- options$fig.path
+      pdf_icon <- file.path(knitr_fig_path, basename(tmp_pdf))
+      if (!dir.exists(d <- dirname(pdf_icon))) dir.create(d, recursive = TRUE)
+      file.copy(tmp_pdf, pdf_icon)
+      knitr::asis_output(sprintf("\\includegraphics[height=0.8em]{%s}", pdf_icon))
+    } else {
+      warning("fontawesome does not supported this output. Icon(s) will not show.",
+              call. = FALSE)
+      ""
+    }
   }
-
   # nocov end
 }
