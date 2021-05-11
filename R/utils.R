179059063766fbe4789e54f388d8e9caadd1e5f4
Carson <cpsievert1@gmail.com>
2021-05-11 17:38:04 -0500

    Close #58: have fa() return a htmltools::tag() object

--- R/utils.R
+++ R/utils.R
@@ -4,4 +4,9 @@
   if (is.null(x)) y else x
 }
 
+extract_group <- function(x, pattern, which = 1) {
+  matches <- regmatches(x, regexec(pattern, x))
+  na.omit(sapply(matches, "[", which + 1))
+}
+
 # nocov end
